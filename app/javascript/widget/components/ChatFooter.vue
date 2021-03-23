0a990eee9fb4ffa6d9386593316e774d3a8dd888
successful-fella <61019742+successful-fella@users.noreply.github.com>
2021-03-23 19:12:56 +0530

    Update ChatFooter.vue

--- app/javascript/widget/components/ChatFooter.vue
+++ app/javascript/widget/components/ChatFooter.vue
@@ -52,7 +52,7 @@ export default {
 .branding {
   align-items: center;
   color: $color-body;
-  display: flex;
+  display: none;
   font-size: $font-size-default;
   justify-content: center;
   padding: $space-one;
